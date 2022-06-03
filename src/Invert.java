c2172dbad77c5df206753117626279370e301362
María Fernández de la Rosa <100228799+7mariadlrosa@users.noreply.github.com>
2022-06-03 13:10:20 +0200

    Add files via upload

---
+++ src/Invert.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+
+public class Invert {
+    public static void main(String[] argv) throws Exception {
+        BigDecimal decimal = new BigDecimal("1234");
+        System.out.println(reverseSign(decimal));
+    }
+    public static final BigDecimal MINUS_ONE = BigDecimal.valueOf(-1L);
+
+    public static BigDecimal reverseSign(BigDecimal decimal) {
+        return minus(decimal);
+    }
+
+    public static BigDecimal minus(BigDecimal decimal) {
+        return decimal.multiply(MINUS_ONE);
+    }
+}
+
